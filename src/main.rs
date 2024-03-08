8d72ebda2c2b4d9092024d62da7167f9aa2d2446
Danni Stjernegård <danni.stjernegard@bulderbank.no>
2024-03-08 09:36:59 +0100

    Respond with OK

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+
+
+                match stream.write("HTTP/1.1 200 OK\r\n\r\n".as_bytes()) {
+                    Ok(_ok) => {
+                        print!("responded with OK");
+                    }
+
+                    Err(e) => {
+                        println!("error: {}", e);
+                    }
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
