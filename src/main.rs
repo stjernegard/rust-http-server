4d3486abdfce8ea65dfcdba4ebe5051dd2c2bb28
Danni Stjernegård <danni.stjernegard@bulderbank.no>
2024-05-02 12:56:23 +0200

    Respond with body content

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{io::{BufRead, BufReader, Error, Write}, net::{TcpListener, TcpStream}};
+use std::{io::{BufRead, BufReader, BufWriter, Error, Write}, net::{TcpListener, TcpStream}};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
@@ -18,7 +18,18 @@ fn main() {
 fn handle(stream: &mut TcpStream) -> Result<(), Error> {
     let request = parse(stream)?;
     let response = respond(request);
-    return write!(stream, "{} {}\r\n\r\n", response.version, response.code);
+
+    let mut writer = BufWriter::new(stream);
+    writer.write_fmt(format_args!("{} {}\r\n", response.version, response.code))?;
+    for (key, value) in response.headers {
+        writer.write_fmt(format_args!("{}: {}\r\n", key, value))?;
+    }
+    if let Some(content) = response.content {
+        writer.write_fmt(format_args!("\r\n{}", content))?;
+    }
+    writer.write(b"\r\n\r\n")?;
+
+    writer.flush()
 }
 
 fn parse(stream: &TcpStream) -> Result<Request, Error> {
@@ -26,23 +37,36 @@ fn parse(stream: &TcpStream) -> Result<Request, Error> {
     let mut reader = BufReader::new(stream);
     reader.read_line(&mut line)?;
     let mut parts = line.split_whitespace();
-    return Ok(Request {
+
+    Ok(Request {
         method: parts.next().unwrap().to_string(),
         path: parts.next().unwrap().to_string(),
         version: parts.next().unwrap().to_string(),
-    });
+    })
 }
 
 fn respond(request: Request) -> Response {
-    let code = if request.method == "GET" && request.path == "/" {
-        "200 OK"
-    } else {
-        "404 Not Found"
-    };
+    let mut code = "404 Not Found";
+    let mut headers: Vec<(String, String)> = Vec::new();
+    let mut content: Option<String> = None;
 
-    return Response {
+    if request.method == "GET" {
+        if request.path == "/" {
+            code = "200 OK"
+        } else if request.path.starts_with("/echo/") {
+            code = "200 OK";
+            let message = request.path.trim_start_matches("/echo/");
+            content = Some(message.to_string());
+            headers.push(("Content-Type".to_string(), "text/plain".to_string()));
+            headers.push(("Content-Length".to_string(), message.len().to_string()));
+        }
+    }
+
+    Response {
         version: request.version,
         code: code.to_string(),
+        headers,
+        content,
     }
 }
 
@@ -55,4 +79,6 @@ struct Request {
 struct Response {
     version: String,
     code: String,
+    headers: Vec<(String, String)>,
+    content: Option<String>,
 }
