698cdafe8161a34c9639b54bfe803f881d6e5aa6
Danni Stjernegård <danni.stjernegard@bulderbank.no>
2024-05-03 15:03:50 +0200

    More refactoring

--- src/main.rs
+++ src/main.rs
@@ -3,53 +3,38 @@ mod response;
 mod router;
 mod server;
 
-use router::Router;
 use server::Server;
 use response::ResponseCode;
 
-use std::net::TcpListener;
-
 fn main() {
-    let listener = match TcpListener::bind("127.0.0.1:4221") {
-        Ok(listener) => listener,
-        Err(error) => {
-            println!("Failed binding to port: {}", error);
-            return
-        },
-    };
-
-    let mut server = Server {
-        listener,
-        router: Router::new(),
-    };
-
-    server.router.register_handler(|req| {
-        req.build_response(
-            ResponseCode::OK,
-            None,
-            None
-        )
-    });
-
-    server.router.register_path("user-agent", |router| {
+    Server::new(|router| {
         router.register_handler(|req| {
             req.build_response(
                 ResponseCode::OK,
                 None,
-                req.headers.get("User-Agent").cloned()
+                None
             )
         });
-    });
 
-    server.router.register_path("echo", |router| {
-        router.register_catchall(|path, req| {
-            req.build_response(
-                ResponseCode::OK,
-                None,
-                Some(path)
-            )
+        router.register_path("user-agent", |router| {
+            router.register_handler(|req| {
+                req.build_response(
+                    ResponseCode::OK,
+                    None,
+                    req.headers.get("User-Agent").cloned()
+                )
+            });
         });
-    });
 
-    server.listen()
+        router.register_path("echo", |router| {
+            router.register_catchall(|path, req| {
+                req.build_response(
+                    ResponseCode::OK,
+                    None,
+                    Some(path)
+                )
+            });
+        });
+
+    }).listen();
 }
--- src/server.rs
+++ src/server.rs
@@ -3,13 +3,26 @@ use std::{io::{BufWriter, Error, Write}, net::{TcpListener, TcpStream}, thread};
 use crate::{request::Request, response::Response, router::Router};
 
 pub struct Server {
-    pub listener: TcpListener,
     pub router: Router,
 }
 
 impl Server {
+    pub fn new(f: fn(&mut Router) -> ()) -> Server {
+        let mut router = Router::new();
+        f(&mut router);
+        Server { router }
+    }
+
     pub fn listen(&self) {
-        for stream in self.listener.incoming() {
+        let listener = match TcpListener::bind("127.0.0.1:4221") {
+            Ok(listener) => listener,
+            Err(error) => {
+                println!("Failed binding to port: {}", error);
+                return
+            },
+        };
+
+        for stream in listener.incoming() {
             let router = self.router.clone();
             thread::spawn(move || {
                 let mut stream = match stream {
